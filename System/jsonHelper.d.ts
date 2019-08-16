74494cef59685cff69196887ef62eb9d35c56e9d
valentinbdv <valentin.pichard@bondevisite.fr>
2019-08-16 10:03:46 +0200

    add jsonHelper

---
+++ System/jsonHelper.d.ts
@@ -0,0 +1,6 @@
+export declare let JsonHelper: {
+    recursiveJsonToObject(interfaceObject: Object, objectToTransale: Object): any;
+    recursiveObjectToJson(interfaceObject: Object, objectToTransale: Object): {};
+    recursiveObjectToObject(interfaceObject: Object, objectToTransale: Object): {};
+    limitAccuracy(number: number, length: number): number;
+};
