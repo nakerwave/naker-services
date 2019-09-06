d398bfd1abb03d37f3cf90b4d2f9e3e2b6943045
valentinbdv <valentin.pichard@bondevisite.fr>
2019-09-06 11:39:03 +0200

    delete build files

--- System/jsonHelper.d.ts
+++
@@ -1,6 +0,0 @@
-export declare let JsonHelper: {
-    recursiveJsonToObject(interfaceObject: Object, objectToTransale: Object): any;
-    recursiveObjectToJson(interfaceObject: Object, objectToTransale: Object): {};
-    recursiveObjectToObject(interfaceObject: Object, objectToTransale: Object): {};
-    limitAccuracy(number: number, length: number): number;
-};
