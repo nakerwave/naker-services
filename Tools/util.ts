26a2d4a99df5f1f5bf09284f3eeef4449df660a1
Valentin <pichou@naker.io>
2021-02-09 23:21:22 +0100

    improve visible element check

--- Tools/util.ts
+++ Tools/util.ts
@@ -1,11 +1,15 @@
 export function checkElementVisible(element: HTMLElement, margin?: number): boolean {
+    if (element == document.body) return true;
     var rect = element.getBoundingClientRect();
     var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
     // is canvas on screen depending on scroll
     if (!margin) margin = 0;
-    let onScreen = !(rect.bottom < -margin || rect.top - viewHeight >= margin);
+    let onScreen = !(rect.bottom <= -margin || rect.top - viewHeight >= margin);
     // is parent display or not
-    let parentDisplayed = !!element.offsetParent;
+    let parentDisplayed = true;
+    if (element.offsetParent) {
+        parentDisplayed = !!element.offsetParent
+    }
     let containerVisible = (onScreen && parentDisplayed);
     return containerVisible;
 }
